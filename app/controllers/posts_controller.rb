f121db47a404e014e0ba41b83154e5d8a592a588
mikitakezawa <mikiaaamiki@yahoo.co.jp>
2023-05-16 22:41:38 +0900

    postモデルにバリデーション作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,37 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+      @post = Post.new
+  end
+  
+  def create
+      @post = Post.new(post_params)
+      if @post.save
+          redirect_to posts_path,notice: 'つぶやきを作成しました!'
+      else
+          render :new
+      end
   end
 
   def edit
+      @post = Post.find(params[:id])
+  end
+  
+  def update
+      @post = post.find(params[:id])
+      if @post.update(post_params)
+          redirect_to posts_path,notice: 'つぶやきを編集しました'
+      else
+          render :edit
+      end
+  end
+  
+  private
+  
+  def post_params
+      params.require(:post).permit(:content)
   end
 end
